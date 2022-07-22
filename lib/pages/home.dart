eeff6bd19b27d6d2576138a6312c2ae8bed3785b
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-07-22 21:19:09 +0600

    add link to quran audio page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quran_app/pages/quran.dart';
 import 'package:quran_app/widgets/prayer_time.dart';
 
 class Home extends StatelessWidget {
@@ -13,8 +14,22 @@ class Home extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 8.0),
         child: Column(
-          children: const [
-            PrayerTime(),
+          children: [
+            const PrayerTime(),
+            Container(
+              margin:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+              padding: const EdgeInsets.only(left: 30, right: 10, top: 10),
+              width: double.infinity,
+              child: ListTile(
+                title: const Text('Quran'),
+                trailing: Icon(Icons.arrow_forward_ios),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const Quran()));
+                },
+              ),
+            ),
           ],
         ),
       ),
