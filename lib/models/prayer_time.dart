b6e6ea02c46b0b471328f5cc69fb8da60651fec5
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-07-22 20:31:48 +0600

    remove unnecessery data fields

--- lib/models/prayer_time.dart
+++ lib/models/prayer_time.dart
@@ -380,39 +380,24 @@ class Timings {
     required this.sunrise,
     required this.dhuhr,
     required this.asr,
-    required this.sunset,
     required this.maghrib,
     required this.isha,
-    required this.imsak,
-    required this.midnight,
-    required this.firstthird,
-    required this.lastthird,
   });
 
   final String fajr;
   final String sunrise;
   final dynamic dhuhr;
   final String asr;
-  final String sunset;
   final String maghrib;
   final String isha;
-  final String imsak;
-  final dynamic midnight;
-  final dynamic firstthird;
-  final String lastthird;
 
   factory Timings.fromMap(Map<String, dynamic> json) => Timings(
         fajr: json["Fajr"],
         sunrise: json["Sunrise"],
         dhuhr: json["Dhuhr"],
         asr: json["Asr"],
-        sunset: json["Sunset"],
         maghrib: json["Maghrib"],
         isha: json["Isha"],
-        imsak: json["Imsak"],
-        midnight: json["Midnight"],
-        firstthird: json["Firstthird"],
-        lastthird: json["Lastthird"],
       );
 
   Map<String, dynamic> toMap() => {
@@ -420,12 +405,7 @@ class Timings {
         "Sunrise": sunrise,
         "Dhuhr": dhuhr,
         "Asr": asr,
-        "Sunset": sunset,
         "Maghrib": maghrib,
         "Isha": isha,
-        "Imsak": imsak,
-        "Midnight": midnight,
-        "Firstthird": firstthird,
-        "Lastthird": lastthird,
       };
 }
