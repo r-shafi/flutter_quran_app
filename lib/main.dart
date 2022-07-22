f13869aabb2568027cebaf782cedb4a189ed3761
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-07-22 20:31:31 +0600

    app root

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:quran_app/pages/home.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const Home(),
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+    );
+  }
+}
