72ba31d5e52963be94b2a1f5cdd204f056f0c1cd
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-07-23 11:32:17 +0600

    add method to play pause stop audio

--- lib/pages/quran.dart
+++ lib/pages/quran.dart
@@ -2,7 +2,10 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:quran_app/models/surah_list.dart';
+import 'package:just_audio/just_audio.dart';
+
+import './../models/surah_content.dart';
+import './../models/surah_list.dart';
 
 Future<SurahListModel> fetchSurahList() async {
   final response = await http.get(
@@ -12,7 +15,19 @@ Future<SurahListModel> fetchSurahList() async {
   if (response.statusCode == 200) {
     return SurahListModel.fromMap(json.decode(response.body));
   } else {
-    throw Exception('Failed to load album');
+    throw Exception('Failed to load data!');
+  }
+}
+
+Future<SurahContentModel> fetchSurahContent(int id) async {
+  final response = await http.get(
+    Uri.parse('http://api.alquran.cloud/v1/surah/$id/ar.alafasy'),
+  );
+
+  if (response.statusCode == 200) {
+    return SurahContentModel.fromMap(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load data!');
   }
 }
 
@@ -24,7 +39,60 @@ class Quran extends StatefulWidget {
 }
 
 class _QuranState extends State<Quran> {
+  final player = AudioPlayer();
+
   late Future<SurahListModel> _futureSurahList;
+  late Future<SurahContentModel> _futureSurahContent;
+
+  late ConcatenatingAudioSource _playlist;
+
+  int lastTrack = 0;
+  bool isPlaying = false;
+
+  void audioPlaybackController(int surah) {
+    if (player.playing && lastTrack == surah) {
+      player.pause();
+      setState(() {
+        isPlaying = false;
+      });
+      return;
+    }
+
+    if (!player.playing && lastTrack == surah) {
+      player.play();
+      setState(() {
+        isPlaying = true;
+      });
+      return;
+    }
+
+    if (player.playing && lastTrack != surah) {
+      player.stop();
+      setState(() {
+        isPlaying = false;
+      });
+    }
+
+    _futureSurahContent = fetchSurahContent(surah);
+
+    _futureSurahContent.then((content) async {
+      _playlist = ConcatenatingAudioSource(
+        useLazyPreparation: true,
+        children: content.data.ayahs
+            .map((ayah) => AudioSource.uri(Uri.parse(ayah.audio)))
+            .toList(),
+      );
+
+      await player.setAudioSource(_playlist);
+
+      setState(() {
+        isPlaying = true;
+        lastTrack = surah;
+      });
+
+      await player.play();
+    });
+  }
 
   @override
   void initState() {
@@ -48,7 +116,14 @@ class _QuranState extends State<Quran> {
                       (surah) => ListTile(
                         title: Text(surah.englishName),
                         subtitle: Text(surah.name),
-                        trailing: const Icon(Icons.play_arrow_sharp),
+                        trailing: IconButton(
+                          icon: isPlaying && lastTrack == surah.number
+                              ? const Icon(Icons.pause)
+                              : const Icon(Icons.play_arrow),
+                          onPressed: () {
+                            audioPlaybackController(surah.number);
+                          },
+                        ),
                       ),
                     )
                     .toList());
