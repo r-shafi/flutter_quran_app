64905cc97d0d5e33eab2b80340dea20c1eb283ba
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-10-28 14:04:29 +0600

    update style

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -18,15 +18,62 @@ class Home extends StatelessWidget {
           children: [
             const PrayerTime(),
             Container(
-              padding: const EdgeInsets.only(left: 30, right: 10, top: 10),
+              margin: const EdgeInsets.symmetric(vertical: 15),
+              padding: const EdgeInsets.all(10),
               width: double.infinity,
-              child: ListTile(
-                title: const Text('Quran'),
-                trailing: const Icon(Icons.arrow_forward_ios),
+              child: GestureDetector(
                 onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const Quran()));
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Quran(),
+                    ),
+                  );
                 },
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.asset(
+                        'assets/images/quran.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.only(
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10),
+                          ),
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.black.withOpacity(0.8),
+                              Colors.black.withOpacity(0.3),
+                              Colors.black.withOpacity(0.01),
+                            ],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                          ),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 30,
+                          horizontal: 20,
+                        ),
+                        child: const Text(
+                          'Quran',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
