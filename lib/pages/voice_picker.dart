6b7741500418656b6cba25bb58be8b8dadbc3eca
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-10-29 11:27:24 +0600

    create page

---
+++ lib/pages/voice_picker.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VoicePicker extends StatefulWidget {
+  const VoicePicker({Key? key}) : super(key: key);
+
+  @override
+  State<VoicePicker> createState() => _VoicePickerState();
+}
+
+class _VoicePickerState extends State<VoicePicker> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
