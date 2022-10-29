ff5a3c3c59630c8e6c72ae6d8631deb682498208
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-10-29 11:56:33 +0600

    fetch qari data, display in gid

--- lib/pages/voice_picker.dart
+++ lib/pages/voice_picker.dart
@@ -1,4 +1,20 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:quran_app/models/audio_list.dart';
+
+Future<AudioListModel> fetchVoiceList() async {
+  final response = await http.get(
+    Uri.parse('https://api.alquran.cloud/v1/edition/format/audio'),
+  );
+
+  if (response.statusCode == 200) {
+    return AudioListModel.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load data!');
+  }
+}
 
 class VoicePicker extends StatefulWidget {
   const VoicePicker({Key? key}) : super(key: key);
@@ -8,8 +24,39 @@ class VoicePicker extends StatefulWidget {
 }
 
 class _VoicePickerState extends State<VoicePicker> {
+  late Future<AudioListModel> _futureVoiceList;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureVoiceList = fetchVoiceList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Select Qari Voice'),
+        centerTitle: true,
+      ),
+      body: FutureBuilder(
+        future: _futureVoiceList,
+        builder: ((context, AsyncSnapshot<AudioListModel> snapshot) {
+          if (snapshot.hasData) {
+            return GridView.count(
+              crossAxisCount: 2,
+              children: snapshot.data!.data.map((e) {
+                return Text(e.englishName);
+              }).toList(),
+            );
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }),
+      ),
+    );
   }
 }
