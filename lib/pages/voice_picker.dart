1f3dc0607b762ff3243388fd7f1b0c27bfa57afe
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-10-29 12:44:45 +0600

    handle voice playing and error

--- lib/pages/voice_picker.dart
+++ lib/pages/voice_picker.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:just_audio/just_audio.dart';
 import 'package:quran_app/models/audio_list.dart';
 
 Future<AudioListModel> fetchVoiceList() async {
@@ -25,11 +26,57 @@ class VoicePicker extends StatefulWidget {
 
 class _VoicePickerState extends State<VoicePicker> {
   late Future<AudioListModel> _futureVoiceList;
+  final player = AudioPlayer();
+
+  String selectedVoice = '';
 
   @override
   void initState() {
     super.initState();
     _futureVoiceList = fetchVoiceList();
+  }
+
+  @override
+  void dispose() {
+    player.stop();
+    player.dispose();
+    super.dispose();
+  }
+
+  void playAudio(String identifier) async {
+    if (player.playing && identifier == selectedVoice) {
+      player.pause();
+      return;
+    } else if (!player.playing && identifier == selectedVoice) {
+      player.play();
+      return;
+    }
+
+    if (player.playing) {
+      player.stop();
+    }
+
+    setState(() {
+      selectedVoice = identifier;
+    });
+
+    try {
+      await player.setUrl(
+        "https://cdn.islamic.network/quran/audio/128/$identifier/262.mp3",
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          backgroundColor: Colors.deepOrange,
+          content: Text(
+            'Something went wrong! Try Selecting Another Voice',
+          ),
+        ),
+      );
+      return;
+    }
+
+    await player.play();
   }
 
   @override
@@ -50,16 +97,21 @@ class _VoicePickerState extends State<VoicePicker> {
                 childAspectRatio: 16 / 5,
                 mainAxisSpacing: 10,
                 crossAxisSpacing: 10,
-                children: snapshot.data!.data.map((e) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: Colors.black12,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: Text(
-                        e.englishName,
-                        textAlign: TextAlign.center,
+                children: snapshot.data!.data.map((voice) {
+                  return GestureDetector(
+                    onTap: () => playAudio(voice.identifier),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: selectedVoice == voice.identifier
+                            ? Colors.deepOrange
+                            : Colors.black12,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text(
+                          voice.englishName,
+                          textAlign: TextAlign.center,
+                        ),
                       ),
                     ),
                   );
