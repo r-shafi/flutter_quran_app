d06f475d041f5ea463da7c0e47dc143231ad66a1
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-11-26 19:01:54 +0600

    init location setter

---
+++ lib/pages/location_setter.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LocationSetter extends StatelessWidget {
+  const LocationSetter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
