23b0e849e650217da53103392fee72c7a8ab5d4d
Shafi Rayhan <rayhanshafi7@gmail.com>
2026-03-15 19:35:07 +0600

    feat: Enable audio service and add necessary permissions in AndroidManifest

--- android/app/src/main/kotlin/com/example/quran_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/quran_app/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.quran_app
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: AudioServiceActivity() {
 }
